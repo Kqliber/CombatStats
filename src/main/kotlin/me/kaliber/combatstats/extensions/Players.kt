37ff60f7000cec2c5902907ea6cfdeb6cfd43887
Kqliber <Dylansaidler03@gmail.com>
2021-03-15 16:24:39 +0000

    removed unused functions

--- src/main/kotlin/me/kaliber/combatstats/extensions/Players.kt
+++ src/main/kotlin/me/kaliber/combatstats/extensions/Players.kt
@@ -1,18 +1,7 @@
 package me.kaliber.combatstats.extensions
 
-import org.bukkit.entity.Player
 import org.bukkit.OfflinePlayer
 import org.bukkit.command.CommandSender
-
-fun Player.msg(message: List<String>)
-{
-    message.forEach { msg(it) }
-}
-
-fun Player.msg(message: String)
-{
-    sendMessage(setMessage(this, message))
-}
 
 fun CommandSender.msg(player: OfflinePlayer, message: List<String>)
 {
