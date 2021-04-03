d15ac05bc73800c8d41cfb5096396f85eaa759db
Kqliber <Dylansaidler03@gmail.com>
2021-04-04 00:40:11 +0100

    fixed a NoSuchMethodError in 1.8

--- src/main/kotlin/me/kaliber/combatstats/user/User.kt
+++ src/main/kotlin/me/kaliber/combatstats/user/User.kt
@@ -3,6 +3,7 @@ package me.kaliber.combatstats.user
 import org.bukkit.OfflinePlayer
 import org.bukkit.Statistic
 import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import java.util.UUID
 
 data class User(val uuid: UUID, var killstreak: Int, var lastKill: String)
@@ -23,13 +24,45 @@ data class User(val uuid: UUID, var killstreak: Int, var lastKill: String)
         killstreak = 0
     }
 
+    /**
+     * 1.8 only supports getting statistics from a Player instead of an OfflinePlayer.
+     * anything above will allow for getting statistics even when the player is offline.
+     */
     fun kills(): Int
     {
-        return player().getStatistic(Statistic.PLAYER_KILLS)
+        val player = player()
+        try
+        {
+            return player.getStatistic(Statistic.PLAYER_KILLS)
+        }
+        catch (exception: NoSuchMethodError)
+        {
+            if (player.isOnline)
+            {
+                return (player as Player).getStatistic(Statistic.PLAYER_KILLS)
+            }
+        }
+        return 0
     }
 
+    /**
+     * 1.8 only supports getting statistics from a Player instead of an OfflinePlayer.
+     * any version above will allow for getting statistics even when the player is offline.
+     */
     fun deaths(): Int
     {
-        return player().getStatistic(Statistic.DEATHS)
+        val player = player()
+        try
+        {
+            return player.getStatistic(Statistic.DEATHS)
+        }
+        catch (exception: NoSuchMethodError)
+        {
+            if (player.isOnline)
+            {
+                return (player as Player).getStatistic(Statistic.DEATHS)
+            }
+        }
+        return 0
     }
 }
